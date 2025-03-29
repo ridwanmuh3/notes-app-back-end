e5ca1073b03bd1b6bc64c3072dd7d04edf5196e1
ridwanmuh3 <ridwanmuhraihan@gmail.com>
2025-03-30 04:39:30 +0700

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import js from '@eslint/js';
+import daStyle from 'eslint-config-dicodingacademy';
+
+export default defineConfig([
+  daStyle,
+  { files: ['**/*.{js,mjs,cjs}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+]);
